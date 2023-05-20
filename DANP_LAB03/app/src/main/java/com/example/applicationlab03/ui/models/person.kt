a81a4c71a6fd9b1bfbe3703ea5b0e1df4b8d5771
José Miguel <jcanov@unsa.edu.pe>
2023-05-20 10:34:15 -0500

    created Person and PersonRepository

---
+++ DANP_LAB03/app/src/main/java/com/example/applicationlab03/ui/models/person.kt
@@ -0,0 +1,10 @@
+package com.example.applicationlab03.ui.models
+
+data class Person(
+    val fullName: String,
+    val registrationDate: String,
+    val bloodType: String,
+    val phone: String,
+    val email: String,
+    val amountPaid: Double
+)
